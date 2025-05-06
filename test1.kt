2bf9cf9799b3fffd3bda9d87c7cdc717d026d6a6
Fanny Pastor <40525222+netfanely@users.noreply.github.com>
2025-05-06 11:36:48 -0500

    Update test1.kt

--- test1.kt
+++ test1.kt
@@ -1,3 +1,34 @@
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.android.AndroidDriver
-im
+import io.appium.java_client.remote.MobileCapabilityType
+import org.openqa.selenium.By
+import org.openqa.selenium.remote.DesiredCapabilities
+import java.net.URL
+
+fun main(){
+  // Configuracion de DesiredCapabilities
+  val capabilities = DesiredCapabilities().apply{
+    setCapability(MobileCapabilityType.PLATFORM_NAME,"Android")
+    setCapability(MobileCapabilityType.DEVICE_NAME,"emulator-5554") // Nombre del dispositivo/emulador
+    setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome") // Usar navegador Chrome
+    setCapability("chromedriverExecutable","/path/to/chromedriver") // Ruta al Chromedriver
+  }
+
+  //inicializar el AppiumDriver
+  val driver: AppiumDriver<*>= AndroidDriver(URL("http://localhost:4723/wd/hub"), capabilities)
+
+  try{
+    //Acceder a google
+    driver.get("https://www.google.com")
+
+    //Esperar a que el campo de búsqueda esté presente y buscar "Avengers"
+    val searchBox = driver.findElement(By.name("q"))
+    searchBox.sendKeys("Avengers")
+    searchBox.submit()
+
+    //Puedes añadir más interacciones aquí según lo requieras
+  } finally {
+    // Cerrar la sesión del driver
+    driver.quit()
+  }
+}
